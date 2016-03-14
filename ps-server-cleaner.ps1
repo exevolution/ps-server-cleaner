0aea781f5079c614095e181a3fddd3afafcc799c
exevolution <exevolution@gmail.com>
2016-03-14 04:54:52 -0700

    Update ps-server-cleaner.ps1

--- ps-server-cleaner.ps1
+++ ps-server-cleaner.ps1
@@ -1,6 +1,8 @@
-﻿# ServerCleanup.ps1
-# Cleans PennyMac servers before and after patch night
-# Contact Elliott Berglund x8981 if you have any issues
+#requires -Version 3.0
+#requires -RunAsAdministrator
+# ps-server-cleaner.ps1
+# Cleans servers before and after patch night
+# Contact ExEvolution http://www.reddit.com/user/exevolution if you have any issues
 Clear-Host
 
 # Configuration options
